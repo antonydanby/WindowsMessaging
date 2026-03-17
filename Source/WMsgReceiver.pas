859b0231be31e47a9f1a3b61970f8ed570a0b95b
zizzo81 <zizzo81@gmail.com>
2026-03-17 16:54:28 +0100

    Managed UIPI under Vista+

--- Source/WMsgReceiver.pas
+++ Source/WMsgReceiver.pas
@@ -52,7 +52,9 @@ type
   TReceivedMessage = procedure(aID: cardinal; aMsg: string) of object;
 
   TWMsgReceiver = class(TComponent)
-  protected
+  private
+    fActive: boolean;
+    fLowProcessMessages: boolean;
     fWindowName: string;
     fWndHandle: HWND;
     fOnMessageReceived: TReceivedMessage;
@@ -62,8 +64,13 @@ type
     procedure CreateTheWindow(const aWindowName: string); overload;
     procedure DestroyWindowAndDeRegister;
 
+    function GetActive: boolean;
     function GetWindowName: string;
+    procedure SetActive(const Value: boolean);
     procedure SetWindowName(const Value: string);
+    procedure SetLowProcessMessages(const Value: boolean);
+  protected
+    procedure Restart;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -71,7 +78,9 @@ type
 
     procedure Start;
     procedure Stop;
- published
+  published
+    property Active: boolean read GetActive write SetActive;
+    property LowProcessMessages: boolean read fLowProcessMessages write SetLowProcessMessages;
     property WindowName: string read GetWindowName write SetWindowName;
     property OnMessageReceived: TReceivedMessage read fOnMessageReceived write fOnMessageReceived;
   end;
@@ -82,6 +91,47 @@ implementation
 
 uses
   WMsgCommon;
+
+const
+  MSGFLT_ADD = 1;
+  MSGFLT_REMOVE = 2;
+
+var
+  ChangeWindowMessageFilter: function(message: UINT; dwFlag: DWORD): BOOL; stdcall;
+  ChangeWindowMessageFilterEx: function(hWnd: HWND; message: UINT; action: DWORD; pChangeFilterStruct: Pointer): BOOL; stdcall;
+
+function EnableLowProcessMessages(hWnd: HWND; Enable: Boolean): Boolean;
+var
+  hUser32: HMODULE;
+  dwMode: Cardinal;
+begin
+  Result := False;
+  hUser32 := GetModuleHandle(user32);
+
+  dwMode := MSGFLT_ADD;
+  if not Enable then
+    dwMode := MSGFLT_REMOVE;
+
+  // 7+ setting is per-window
+  if not Assigned(ChangeWindowMessageFilterEx) then
+    ChangeWindowMessageFilterEx := GetProcAddress(hUser32, 'ChangeWindowMessageFilterEx');
+
+  if Assigned(ChangeWindowMessageFilterEx) then
+  begin
+
+    Result := ChangeWindowMessageFilterEx(hWnd, WM_COPYDATA, dwMode, nil);
+  end;
+
+  if Result then
+    Exit;
+
+  // Vista+ setting is per process
+  if not Assigned(ChangeWindowMessageFilter) then
+    ChangeWindowMessageFilter := GetProcAddress(hUser32, 'ChangeWindowMessageFilter');
+
+  if Assigned(ChangeWindowMessageFilter) then
+    Result := ChangeWindowMessageFilter(WM_COPYDATA, dwMode)
+end;
 
 { TReceiver }
 
@@ -95,6 +145,9 @@ begin
   inherited Create(AOwner);
   // Initializing the field to ensure we have a window name at construction
   fWindowName := WMSG_DEFAULT_WINDOW;
+  // Make sure to clear the variables.
+  fActive := false;
+  fWndHandle := 0;
 end;
 
 destructor TWMsgReceiver.Destroy;
@@ -106,11 +159,17 @@ end;
 procedure TWMsgReceiver.DestroyWindowAndDeRegister;
 begin
   if fWndHandle <> 0 then DestroyWindow(fWndHandle);
+  // Ensure to reset the handle to zero.
+  fWndHandle := 0;
   Winapi.Windows.UnregisterClass(WINDOWS_MESSAGING_CLASS_NAME, HInstance);
 end;
 
 procedure TWMsgReceiver.Start;
 begin
+  // Avoid creating the window multiple times.
+  if Active then
+    Exit;
+
   if fWindowName.IsEmpty then fWindowName := WMSG_DEFAULT_WINDOW;
   CreateTheWindow(fWindowName);
 end;
@@ -140,9 +199,13 @@ begin
     raise Exception.Create('Failed to create message window');
 
   SetWindowLongPtr(fWndHandle, GWLP_USERDATA, LONG_PTR(Self));
+
+  // If necessary, enable messages through UIPI.
+  if LowProcessMessages then
+    EnableLowProcessMessages(fWndHandle, True)
 end;
 
-class function TWMsgReceiver.StaticWndProc(hWnd: HWND; uMsg: UINT; wParam: WPARAM; lParam: LPARAM): LRESULT; stdcall;
+class function TWMsgReceiver.StaticWndProc(hWnd: HWND; uMsg: UINT; wParam: WPARAM; lParam: LPARAM): LRESULT;
 var
   Receiver: TWMsgReceiver;
   Msg: TMessage;
@@ -175,6 +238,11 @@ begin
   else aMsg.Result := DefWindowProc(fWndHandle, aMsg.Msg, aMsg.WParam, aMsg.LParam);
 end;
 
+function TWMsgReceiver.GetActive: boolean;
+begin
+  Result := fWndHandle > 0
+end;
+
 function TWMsgReceiver.GetHandle: HWND;
 begin
   Result := fWndHandle;
@@ -186,6 +254,49 @@ begin
   Result := fWindowName;
 end;
 
+procedure TWMsgReceiver.Restart;
+var
+  Running: boolean;
+begin
+  // If not active does nothing.
+  if not Active then
+    Exit;
+
+  // Store current status.
+  Running := Active;
+
+  // Stop everything.
+  Active := False;
+
+  // If it was running, restart it.
+  if Running then
+    Active := True
+end;
+
+procedure TWMsgReceiver.SetActive(const Value: boolean);
+begin
+  // Don't do this while in Delphi IDE, runtime only.
+  if (Value <> Active) and not(csDesigning in ComponentState) then
+    if Value then
+      Start
+    else
+      Stop
+end;
+
+procedure TWMsgReceiver.SetLowProcessMessages(const Value: boolean);
+begin
+  // Only act if changing it.
+  if Value = fLowProcessMessages then
+    Exit;
+
+  // Changes the value.
+  fLowProcessMessages := Value;
+
+  // Only applies it effectively on
+  if not (csDesigning in ComponentState) then
+    Restart
+end;
+
 procedure TWMsgReceiver.SetWindowName(const Value: string);
 begin
   // Setters are perfect for validation or side effects
@@ -193,6 +304,3 @@ begin
 end;
 
 end.
-
-
-
